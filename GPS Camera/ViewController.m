fc6889f0ea0a05f10e80f776a8fbf966502a95b9
My Mac <mymac@Mys-iMac.local>
2019-04-06 11:43:36 +0530

    Initial Commit

---
+++ GPS Camera/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  GPS Camera
+//
+//  Created by My Mac on 4/6/19.
+//  Copyright © 2019 My Mac. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ GPS Camera/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GPS Camera
+//
+//  Created by My Mac on 4/6/19.
+//  Copyright © 2019 My Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
